b7051f11e3b06bb81bba8c7f193189d4f2113ca3
ISAM ELZOBI <mobarmeg.9@gmail.com>
2024-12-13 17:13:29 +0300

    Add `TTableHeader` widget and update `CategoriesDesktop` screen
    Created a new `TTableHeader` widget for the data table header and updated the `CategoriesDesktop` screen to use this new widget. Also, added a `CategoryTable` widget as a placeholder for the table content. Removed the old `table_header.dart` file as it is no longer needed.

--- lib/features/shop/screens/category/all_categories/responsive_screens/categories_desktop.dart
+++ lib/features/shop/screens/category/all_categories/responsive_screens/categories_desktop.dart
@@ -1,10 +1,56 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:t_ecommerce_admin_panel/common/widgets/appbar/breadcrumbs/breadcrumb_with_heading.dart';
+import 'package:t_ecommerce_admin_panel/common/widgets/containers/rounded_container.dart';
+import 'package:t_ecommerce_admin_panel/common/widgets/data_table/table_header.dart';
+import 'package:t_ecommerce_admin_panel/features/shop/screens/category/all_categories/table/data_table.dart';
+import 'package:t_ecommerce_admin_panel/routes/routes.dart';
+import 'package:t_ecommerce_admin_panel/utils/constants/sizes.dart';
 
 class CategoriesDesktop extends StatelessWidget {
   const CategoriesDesktop({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(TSizes.defaultSpace),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              // Breadcrumbs
+              const TBreadcrumbWithHeading(
+                heading: 'Categories',
+                breadcrumbItems: ['Categories'],
+              ),
+              const SizedBox(height: TSizes.spaceBtwSections),
+
+              // Table Body
+              //   Obx(() {
+              // Show Loader
+              //return
+              TRoundedContainer(
+                child: Column(
+                  children: [
+                    // Table Header
+                    TTableHeader(
+                      buttonText: 'Create New Category',
+                      onPressed: () => Get.toNamed(TRoutes.createCategory),
+
+                    ),
+                    const SizedBox(height: TSizes.spaceBtwItems),
+
+                    // Table
+                    const CategoryTable()
+                  ],
+                ),
+              ),
+              //  })
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
---
+++ lib/features/shop/screens/category/all_categories/table/data_table.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class CategoryTable extends StatelessWidget {
+  const CategoryTable({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}

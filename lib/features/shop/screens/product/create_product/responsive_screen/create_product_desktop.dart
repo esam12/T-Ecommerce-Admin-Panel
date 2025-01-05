9da9182ba0f26e4d9e54c96b29dc955be811e885
ISAM ELZOBI <mobarmeg.9@gmail.com>
2025-01-05 22:58:07 +0300

    Implement Create Product Screen Layout for Desktop and Mobile
    Added the layout for the Create Product screen for both desktop and mobile views. Included breadcrumbs, sections for basic information, stock & pricing, attributes, variations, product images, brand, categories, and visibility. Used `TRoundedContainer` for styling sections and `TSizes` for consistent spacing.

--- lib/features/shop/screens/product/create_product/responsive_screen/create_product_desktop.dart
+++ lib/features/shop/screens/product/create_product/responsive_screen/create_product_desktop.dart
@@ -1,10 +1,125 @@
 import 'package:flutter/material.dart';
+import 'package:t_ecommerce_admin_panel/common/widgets/appbar/breadcrumbs/breadcrumb_with_heading.dart';
+import 'package:t_ecommerce_admin_panel/common/widgets/containers/rounded_container.dart';
+import 'package:t_ecommerce_admin_panel/routes/routes.dart';
+import 'package:t_ecommerce_admin_panel/utils/constants/sizes.dart';
+import 'package:t_ecommerce_admin_panel/utils/device/device_utility.dart';
 
 class CreateProductDesktopScreen extends StatelessWidget {
   const CreateProductDesktopScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      // bottomNavigationBar: const ProductBottomNavigationButtons(),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(TSizes.defaultSpace),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              // Breadcrumbs
+              const TBreadcrumbWithHeading(
+                returnToPreviousScreen: true,
+                heading: 'Create Product',
+                breadcrumbItems: [TRoutes.products, 'Create Product'],
+              ),
+              const SizedBox(height: TSizes.spaceBtwSections),
+
+              // Create Product
+              Row(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Expanded(
+                    flex: TDeviceUtils.isTabletScreen(context) ? 2 : 3,
+                    child: Column(
+                      children: [
+                        // Basic Information
+                        // const ProductTitleAndDescription(),
+                        const SizedBox(height: TSizes.spaceBtwSections),
+
+                        // Stock & Pricing
+                        TRoundedContainer(
+                          child: Column(
+                            children: [
+                              // Heading
+                              Text('Stock & Pricing',
+                                  style: Theme.of(context)
+                                      .textTheme
+                                      .headlineSmall),
+                              const SizedBox(height: TSizes.spaceBtwItems),
+
+                              // Product Type
+                              // const ProductTypeWidget(),
+                              const SizedBox(height: TSizes.spaceBtwItems),
+
+                              // Stock
+                              // const ProductStockAndPricing(),
+                              const SizedBox(height: TSizes.spaceBtwSections),
+
+                              // Attributes
+                              // const ProductAttributes(),
+                              const SizedBox(height: TSizes.spaceBtwSections),
+                            ],
+                          ),
+                        ),
+                        const SizedBox(height: TSizes.spaceBtwSections),
+
+                        // Variations
+                        // const ProductVariations(),
+                      ],
+                    ),
+                  ),
+                  const SizedBox(height: TSizes.defaultSpace),
+
+                  // Sidebar
+                  Expanded(
+                    child: Column(
+                      children: [
+                        // Product Thumbnail
+                        // const ProductThumbnailImage(),
+                        const SizedBox(height: TSizes.spaceBtwSections),
+
+                        // Product Images
+                        TRoundedContainer(
+                          child: Column(
+                            children: [
+                              Text('All Product Images',
+                                  style: Theme.of(context)
+                                      .textTheme
+                                      .headlineSmall),
+                              const SizedBox(height: TSizes.spaceBtwItems),
+
+                              // ProductAdditionalImages(
+                              //   additionalProductImages: RxList<String>.empty(),
+                              //   onTapToAddImages(){},
+                              //   onTapToRemoveImages(index){},
+                              // ),
+                            ],
+                          ),
+                        ),
+                        const SizedBox(height: TSizes.spaceBtwSections),
+
+                        // Product Brand
+                        // const ProductBrand(),
+                        const SizedBox(height: TSizes.spaceBtwSections),
+
+                        // Product Categories
+                        // const ProductCategories(),
+                        const SizedBox(height: TSizes.spaceBtwSections),
+
+                        // Product Visibility
+                        // const ProductVisibility(),
+                        const SizedBox(height: TSizes.spaceBtwSections),
+                      ],
+                    ),
+                  )
+                ],
+              )
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }

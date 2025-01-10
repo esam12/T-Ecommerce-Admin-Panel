33128ac0a48668e50c67af54452e6d9fbbec82cd
ISAM ELZOBI <mobarmeg.9@gmail.com>
2025-01-10 17:43:43 +0300

    Add customer details and address sections to OrderCustomer widget
    Added a new `.vscode/settings.json` file to include custom spell check words. Updated the `OrderCustomer` widget to display customer personal information, contact information, shipping address, and billing address using `TRoundedContainer` and `TRoundedImage` components.

--- lib/features/shop/screens/order/order_details/widgets/order_customer.dart
+++ lib/features/shop/screens/order/order_details/widgets/order_customer.dart
@@ -1,5 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:t_ecommerce_admin_panel/common/widgets/containers/rounded_container.dart';
+import 'package:t_ecommerce_admin_panel/common/widgets/images/t_rounded_image.dart';
 import 'package:t_ecommerce_admin_panel/features/shop/models/order_model.dart';
+import 'package:t_ecommerce_admin_panel/utils/constants/colors.dart';
+import 'package:t_ecommerce_admin_panel/utils/constants/enums.dart';
+import 'package:t_ecommerce_admin_panel/utils/constants/image_strings.dart';
+import 'package:t_ecommerce_admin_panel/utils/constants/sizes.dart';
 
 class OrderCustomer extends StatelessWidget {
   const OrderCustomer({super.key, required this.order});
@@ -9,7 +15,115 @@ class OrderCustomer extends StatelessWidget {
   Widget build(BuildContext context) {
     return Column(
       children: [
-        
+        // Personal Information
+        TRoundedContainer(
+          padding: const EdgeInsets.all(TSizes.defaultSpace),
+          child: Column(
+            children: [
+              Text('Customer',
+                  style: Theme.of(context).textTheme.headlineMedium),
+              const SizedBox(height: TSizes.spaceBtwSections),
+              Row(
+                children: [
+                  const TRoundedImage(
+                    padding: 0,
+                    backgroundColor: TColors.primaryBackground,
+                    imageType: ImageType.asset,
+                    image: TImages.user,
+                  ),
+                  const SizedBox(width: TSizes.spaceBtwItems),
+                  Expanded(
+                    child: Column(
+                      children: [
+                        Text(
+                          'ISAM ELZOBI',
+                          style: Theme.of(context).textTheme.titleLarge,
+                          overflow: TextOverflow.ellipsis,
+                          maxLines: 1,
+                        ),
+                        const Text(
+                          'support@codingwithiso.com',
+                          overflow: TextOverflow.ellipsis,
+                          maxLines: 1,
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
+        const SizedBox(height: TSizes.spaceBtwSections),
+
+        // Contact Information
+        SizedBox(
+          width: double.infinity,
+          child: TRoundedContainer(
+            padding: const EdgeInsets.all(TSizes.defaultSpace),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text('Contact Person',
+                    style: Theme.of(context).textTheme.headlineMedium),
+                const SizedBox(height: TSizes.spaceBtwSections),
+                Text('Coding with ISO',
+                    style: Theme.of(context).textTheme.titleLarge),
+                const SizedBox(height: TSizes.spaceBtwSections),
+                Text('support@codingwithiso.com',
+                    style: Theme.of(context).textTheme.titleLarge),
+                const SizedBox(height: TSizes.spaceBtwSections),
+                Text('+90-***-***-**-**',
+                    style: Theme.of(context).textTheme.titleLarge),
+              ],
+            ),
+          ),
+        ),
+        const SizedBox(height: TSizes.spaceBtwSections),
+
+        // Shipping Address
+        SizedBox(
+          width: double.infinity,
+          child: TRoundedContainer(
+            padding: const EdgeInsets.all(TSizes.defaultSpace),
+            child: Column(
+              children: [
+                Text('Shipping Address',
+                    style: Theme.of(context).textTheme.headlineMedium),
+                const SizedBox(height: TSizes.spaceBtwSections),
+                Text('ISAM ELZOBI',
+                    style: Theme.of(context).textTheme.titleSmall),
+                const SizedBox(height: TSizes.spaceBtwItems / 2),
+                Text(
+                    'Adnan Menderes Caddesi Kayabasi Mah. No: 1 Basaksehir/Istanbul Turkey ',
+                    style: Theme.of(context).textTheme.titleSmall),
+              ],
+            ),
+          ),
+        ),
+        const SizedBox(height: TSizes.spaceBtwSections),
+
+        // Billing Address
+        SizedBox(
+          width: double.infinity,
+          child: TRoundedContainer(
+            padding: const EdgeInsets.all(TSizes.defaultSpace),
+            child: Column(
+              children: [
+                Text('Billing Address',
+                    style: Theme.of(context).textTheme.headlineMedium),
+                const SizedBox(height: TSizes.spaceBtwSections),
+                Text('ISAM ELZOBI',
+                    style: Theme.of(context).textTheme.titleSmall),
+                const SizedBox(height: TSizes.spaceBtwItems / 2),
+                Text(
+                    'Adnan Menderes Caddesi Kayabasi Mah. No: 1 Basaksehir/Istanbul Turkey ',
+                    style: Theme.of(context).textTheme.titleSmall),
+              ],
+            ),
+          ),
+        ),
+        const SizedBox(height: TSizes.spaceBtwSections),
       ],
     );
   }

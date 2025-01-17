1adef48986ea5bb5c46a96de976c8a4dbf645266
ISAM ELZOBI <mobarmeg.9@gmail.com>
2025-01-17 22:27:14 +0300

    Refactor and enhance CategoryRepository and CategoryModel
    - Cleaned up unnecessary comments and formatting in CategoryRepository.
    - Added `empty` constructor and `toJson` method to CategoryModel.
    - Implemented `fromSnapshot` factory constructor in CategoryModel to map Firestore document snapshots to CategoryModel instances.

--- lib/data/repositories/category/category_repository.dart
+++ lib/data/repositories/category/category_repository.dart
@@ -5,20 +5,19 @@ import 'package:t_ecommerce_admin_panel/features/shop/models/category_model.dart
 import 'package:t_ecommerce_admin_panel/utils/exceptions/firebase_auth_exceptions.dart';
 import 'package:t_ecommerce_admin_panel/utils/exceptions/platform_exceptions.dart';
 
-class CategoryRepository extends GetxController { 
+class CategoryRepository extends GetxController {
   static CategoryRepository get instance => Get.find();
 
   // Firebase Firestore instance
   final FirebaseFirestore _db = FirebaseFirestore.instance;
 
-
   // Get all categories from the 'Categories' collection
   Future<List<CategoryModel>> getAllCategories() async {
     try {
       final querySnapshot = await _db.collection('Categories').get();
-      // return querySnapshot.docs
-      //     .map((doc) => CategoryModel.fromSnapshot(doc))
-      //     .toList();
+      return querySnapshot.docs
+          .map((doc) => CategoryModel.fromSnapshot(doc))
+          .toList();
     } on FirebaseException catch (e) {
       throw TFirebaseAuthException(e.code).message;
     } on PlatformException catch (e) {
@@ -27,6 +26,4 @@ class CategoryRepository extends GetxController {
       throw 'Something went wrong. Please try again';
     }
   }
-
-
-} 
+}
--- lib/features/shop/models/category_model.dart
+++ lib/features/shop/models/category_model.dart
@@ -19,4 +19,46 @@ class CategoryModel {
     this.updatedAt,
   });
 
+  /// Create Empty model
+  static CategoryModel empty() => CategoryModel(
+        id: '',
+        name: '',
+        image: '',
+        isFeatured: false,
+        parentId: '',
+        createdAt: null,
+        updatedAt: null,
+      );
+
+  /// Convert model to json structure so that you can store data in Firebase
+  Map<String, dynamic> toJson() => {
+        'id': id,
+        'name': name,
+        'image': image,
+        'isFeatured': isFeatured,
+        'parentId': parentId,
+        'createdAt': createdAt,
+        'updatedAt': updatedAt,
+      };
+
+  /// Map Json oriented document snapshot from Firebase to userModel
+  factory CategoryModel.fromSnapshot(
+      DocumentSnapshot<Map<String, dynamic>> document) {
+    if (document.data() != null) {
+      final data = document.data()!;
+      return CategoryModel(
+        id: document.id,
+        name: data['name'] ?? '',
+        image: data['image'] ?? '',
+        isFeatured: data['isFeatured'] ?? false,
+        parentId: data['parentId'] ?? '',
+        createdAt:
+            data.containsKey('createdAt') ? data['createdAt'].toDate() : null,
+        updatedAt:
+            data.containsKey('updatedAt') ? data['updatedAt'].toDate() : null,
+      );
+    } else {
+      return CategoryModel.empty();
+    }
+  }
 }

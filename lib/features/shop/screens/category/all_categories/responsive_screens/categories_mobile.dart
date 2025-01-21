e2a8bc1cd04fe6932b2a4451036a69428789dc7b
ISAM ELZOBI <mobarmeg.9@gmail.com>
2025-01-21 15:34:03 +0300

    Implement responsive category screens for mobile and tablet
    Updated the `CategoriesMobile` and `CategoriesTablet` screens to include a breadcrumb, table header, search functionality, and a data table. Added loader animation while data is being fetched.

--- lib/features/shop/screens/category/all_categories/responsive_screens/categories_mobile.dart
+++ lib/features/shop/screens/category/all_categories/responsive_screens/categories_mobile.dart
@@ -1,10 +1,66 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:t_ecommerce_admin_panel/common/widgets/appbar/breadcrumbs/breadcrumb_with_heading.dart';
+import 'package:t_ecommerce_admin_panel/common/widgets/containers/rounded_container.dart';
+import 'package:t_ecommerce_admin_panel/common/widgets/data_table/table_header.dart';
+import 'package:t_ecommerce_admin_panel/common/widgets/loaders/loader_animation.dart';
+import 'package:t_ecommerce_admin_panel/features/shop/controllers/category/category_controller.dart';
+import 'package:t_ecommerce_admin_panel/features/shop/screens/category/all_categories/table/data_table.dart';
+import 'package:t_ecommerce_admin_panel/routes/routes.dart';
+import 'package:t_ecommerce_admin_panel/utils/constants/sizes.dart';
 
 class CategoriesMobile extends StatelessWidget {
   const CategoriesMobile({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    final controller = Get.put(CategoryController());
+
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(TSizes.defaultSpace),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              // Breadcrumbs
+              const TBreadcrumbWithHeading(
+                heading: 'Categories',
+                breadcrumbItems: ['Categories'],
+              ),
+              const SizedBox(height: TSizes.spaceBtwSections),
+
+              // Table Body
+              //   Obx(() {
+              // Show Loader
+              //return
+              TRoundedContainer(
+                child: Column(
+                  children: [
+                    // Table Header
+                    TTableHeader(
+                      buttonText: 'Create New Category',
+                      onPressed: () => Get.toNamed(TRoutes.createCategory),
+                      searchController: controller.searchController,
+                      searchOnChanged: (query) => controller.searchItems(query),
+                    ),
+                    const SizedBox(height: TSizes.spaceBtwItems),
+
+                    // Table
+                    Obx(() {
+                      if (controller.isLoading.value) {
+                        return const TLoaderAnimation();
+                      }
+                      return const CategoryTable();
+                    })
+                  ],
+                ),
+              ),
+              //  })
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }

2d76f6338fc035bd930841e0f9cde363a82be02a
ISAM ELZOBI <mobarmeg.9@gmail.com>
2025-02-07 17:14:38 +0300

    Add method to fetch brand categories
    Added a new method `getAllBrandCategories` to the `BrandRepository` class to fetch all brand categories from the 'BrandCategories' collection in Firestore. Also, updated the import statement for `BrandCategoryModel`.

--- lib/data/repositories/brand/brand_repository.dart
+++ lib/data/repositories/brand/brand_repository.dart
@@ -1,18 +1,18 @@
-
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/services.dart';
 import 'package:get/get.dart';
+import 'package:t_ecommerce_admin_panel/features/shop/models/brand_category_model.dart';
 import 'package:t_ecommerce_admin_panel/features/shop/models/brand_model.dart';
 import 'package:t_ecommerce_admin_panel/utils/exceptions/firebase_auth_exceptions.dart';
 import 'package:t_ecommerce_admin_panel/utils/exceptions/platform_exceptions.dart';
 
-class BrandRepository extends GetxController { 
+class BrandRepository extends GetxController {
   static BrandRepository get instance => Get.find();
 
   // Initialize Firebase
   final _db = FirebaseFirestore.instance;
 
-  // Fetch All Brands from Firestore
+  /// Fetch All Brands from Firestore
   Future<List<BrandModel>> fetchAllBrands() async {
     try {
       final querySnapshot = await _db.collection('Brands').get();
@@ -28,6 +28,19 @@ class BrandRepository extends GetxController {
     }
   }
 
-  
-
-} 
+  /// Fetch all brand categories from the 'BrandCategories' collection
+  Future<List<BrandCategoryModel>> getAllBrandCategories() async {
+    try {
+      final querySnapshot = await _db.collection('BrandCategories').get();
+      return querySnapshot.docs
+          .map((doc) => BrandCategoryModel.fromSnapshot(doc))
+          .toList();
+    } on FirebaseException catch (e) {
+      throw TFirebaseAuthException(e.code).message;
+    } on PlatformException catch (e) {
+      throw TPlatformException(e.code).message;
+    } catch (e) {
+      throw 'Something went wrong. Please try again';
+    }
+  }
+}

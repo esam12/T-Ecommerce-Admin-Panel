0f252854b8bd543e62b6c84b2215f023fe4dfa77
ISAM ELZOBI <mobarmeg.9@gmail.com>
2025-03-06 21:45:34 +0300

    feat: Implement category selection with real data and loading state
    - Added GetX controllers for category management and product creation
    - Implemented category fetching functionality
    - Added shimmer loading effect while categories are loading
    - Updated MultiSelectDialogField to use real category data from controller
    - Connected category selection to product controller
    - Improved UI with loading states and dynamic data

--- lib/features/shop/screens/product/create_product/widgets/categories_widget.dart
+++ lib/features/shop/screens/product/create_product/widgets/categories_widget.dart
@@ -1,9 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 import 'package:multi_select_flutter/dialog/multi_select_dialog_field.dart';
 import 'package:multi_select_flutter/util/multi_select_item.dart';
 import 'package:multi_select_flutter/util/multi_select_list_type.dart';
 import 'package:t_ecommerce_admin_panel/common/widgets/containers/rounded_container.dart';
-import 'package:t_ecommerce_admin_panel/features/shop/models/category_model.dart';
+import 'package:t_ecommerce_admin_panel/common/widgets/shimmers/shimmer.dart';
+import 'package:t_ecommerce_admin_panel/features/shop/controllers/category/category_controller.dart';
+import 'package:t_ecommerce_admin_panel/features/shop/controllers/product/create_product_controller.dart';
 import 'package:t_ecommerce_admin_panel/utils/constants/sizes.dart';
 
 class ProductCategories extends StatelessWidget {
@@ -11,6 +14,13 @@ class ProductCategories extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    // Get instance of the CatecoryController
+    final categoriesController = Get.put(CategoryController());
+
+    // Fetch categories if the list is empty
+    if (categoriesController.allItems.isEmpty) {
+      categoriesController.fetchItems();
+    }
     return TRoundedContainer(
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
@@ -23,19 +33,22 @@ class ProductCategories extends StatelessWidget {
           const SizedBox(height: TSizes.spaceBtwItems),
 
           // MultiSelectedDialogField for selecting categories
-          MultiSelectDialogField(
-            buttonText: const Text('Select Categories'),
-            title: const Text('Categories'),
-            items: [
-              MultiSelectItem(
-                  CategoryModel(id: 'id', name: 'Shoes', image: 'image'),
-                  'Shoes'),
-              MultiSelectItem(
-                  CategoryModel(id: 'id', name: 'Shirts', image: 'image'),
-                  'Shirts'),
-            ],
-            listType: MultiSelectListType.CHIP,
-            onConfirm: (values) {},
+          Obx(
+            () => categoriesController.isLoading.value
+                ? const TShimmerEffect(width: double.infinity, height: 50)
+                : MultiSelectDialogField(
+                    buttonText: const Text('Select Categories'),
+                    title: const Text('Categories'),
+                    items: categoriesController.allItems
+                        .map((category) =>
+                            MultiSelectItem(category, category.name))
+                        .toList(),
+                    listType: MultiSelectListType.CHIP,
+                    onConfirm: (values) {
+                      CreateProductController.instance.selectedCategories
+                          .assignAll(values);
+                    },
+                  ),
           )
         ],
       ),

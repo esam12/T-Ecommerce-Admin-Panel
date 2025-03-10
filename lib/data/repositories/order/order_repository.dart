3a0b6d3b6302b6171270847430226467a51710fd
ISAM ELZOBI <mobarmeg.9@gmail.com>
2025-03-10 23:59:45 +0300

    - Created new OrderRepository with methods:   - getAllOrders() to fetch orders sorted by date   - addOrder() to store new orders - Implemented error handling for Firebase, Format, and Platform exceptions

---
+++ lib/data/repositories/order/order_repository.dart
@@ -0,0 +1,48 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/services.dart';
+import 'package:get/get.dart';
+import 'package:t_ecommerce_admin_panel/features/shop/models/order_model.dart';
+import 'package:t_ecommerce_admin_panel/utils/exceptions/firebase_exceptions.dart';
+import 'package:t_ecommerce_admin_panel/utils/exceptions/format_exceptions.dart';
+import 'package:t_ecommerce_admin_panel/utils/exceptions/platform_exceptions.dart';
+
+class OrderRepository extends GetxController {
+  static OrderRepository get instance => Get.find();
+
+  // Firebsae Firestore instance
+  final FirebaseFirestore _db = FirebaseFirestore.instance;
+
+  // Get all orders related to the current user
+  Future<List<OrderModel>> getAllOrders() async {
+    try {
+      final result = await _db
+          .collection('Orders')
+          .orderBy('orderDate', descending: true)
+          .get();
+      return result.docs.map((e) => OrderModel.fromSnapshot(e)).toList();
+    } on FirebaseException catch (e) {
+      throw TFirebaseException(e.code).message;
+    } on FormatException catch (_) {
+      throw const TFormatException();
+    } on PlatformException catch (e) {
+      throw TPlatformException(e.code).message;
+    } catch (e) {
+      throw 'Something went wrong. Please try again';
+    }
+  }
+
+  // Store a new user order
+  Future<void> addOrder(OrderModel order) async {
+    try {
+      await _db.collection('Orders').add(order.toJson());
+    } on FirebaseException catch (e) {
+      throw TFirebaseException(e.code).message;
+    } on FormatException catch (_) {
+      throw const TFormatException();
+    } on PlatformException catch (e) {
+      throw TPlatformException(e.code).message;
+    } catch (e) {
+      throw 'Something went wrong. Please try again';
+    }
+  }
+}

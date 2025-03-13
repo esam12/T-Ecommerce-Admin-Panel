3f3fc81a1cf510af2d41e0445ae7754e31895c6e
ISAM ELZOBI <mobarmeg.9@gmail.com>
2025-03-13 23:51:17 +0300

    refactor: Update orders table source with controller integration
    - Replaced DashboardController with OrderController instance
    - Added dynamic item count display for orders
    - Implemented row selection functionality with controller integration
    - Added order ID parameter in navigation to details
    - Enabled delete functionality through controller
    - Updated row count and selection count to use controller's filtered items
    - Improved navigation consistency with order ID parameter

--- lib/features/shop/screens/order/all_orders/table/table_source.dart
+++ lib/features/shop/screens/order/all_orders/table/table_source.dart
@@ -3,21 +3,23 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:t_ecommerce_admin_panel/common/widgets/containers/rounded_container.dart';
 import 'package:t_ecommerce_admin_panel/common/widgets/icons/table_action_icon_buttons.dart';
-import 'package:t_ecommerce_admin_panel/features/shop/controllers/dashboard/dashboard_controller.dart';
+import 'package:t_ecommerce_admin_panel/features/shop/controllers/order/order_controller.dart';
 import 'package:t_ecommerce_admin_panel/routes/routes.dart';
 import 'package:t_ecommerce_admin_panel/utils/constants/colors.dart';
 import 'package:t_ecommerce_admin_panel/utils/constants/sizes.dart';
 import 'package:t_ecommerce_admin_panel/utils/helpers/helper_functions.dart';
 
 class OrdersRows extends DataTableSource {
+  final controller = OrderController.instance;
   @override
   DataRow? getRow(int index) {
-    final order = DashboardController.orders[index];
+    final order = controller.filteredItems[index];
 
     return DataRow2(
-      onTap: () => Get.toNamed(TRoutes.orderDetails, arguments: order),
-      selected: false,
-      onSelectChanged: (value) {},
+      onTap: () => Get.toNamed(TRoutes.orderDetails,
+          arguments: order, parameters: {'orderId': order.id}),
+      selected: controller.selectRows[index],
+      onSelectChanged: (value) => controller.selectRows[index] = value ?? false,
       cells: [
         DataCell(
           Text(
@@ -29,7 +31,7 @@ class OrdersRows extends DataTableSource {
           ),
         ),
         DataCell(Text(order.formattedOrderDate)),
-        const DataCell(Text('5 Items')),
+        DataCell(Text('${order.items.length} Items')),
         DataCell(
           TRoundedContainer(
             radius: TSizes.cardRadiusSm,
@@ -50,9 +52,9 @@ class OrdersRows extends DataTableSource {
         DataCell(TTableActionButtons(
           view: true,
           edit: false,
-          onViewPressed: () =>
-              Get.toNamed(TRoutes.orderDetails, arguments: order),
-          onDeletePressed: () {},
+          onViewPressed: () => Get.toNamed(TRoutes.orderDetails,
+              arguments: order, parameters: {'orderId': order.id}),
+          onDeletePressed: () => controller.confirmAndDeleteItem(order),
         ))
       ],
     );
@@ -62,8 +64,9 @@ class OrdersRows extends DataTableSource {
   bool get isRowCountApproximate => false;
 
   @override
-  int get rowCount => DashboardController.orders.length;
+  int get rowCount => controller.filteredItems.length;
 
   @override
-  int get selectedRowCount => 0;
+  int get selectedRowCount =>
+      controller.selectRows.where((selected) => selected).length;
 }

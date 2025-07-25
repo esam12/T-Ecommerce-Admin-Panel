7e90010f535660af016d2017018af5532c495fe6
ISAM ELZOBI <mobarmeg.9@gmail.com>
2025-07-25 19:59:29 +0300

    Add CustomerDetailController initialization

--- lib/features/shop/screens/customer/customer_detail/customer_detail.dart
+++ lib/features/shop/screens/customer/customer_detail/customer_detail.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:t_ecommerce_admin_panel/common/widgets/layouts/templates/site_template.dart';
+import 'package:t_ecommerce_admin_panel/features/shop/controllers/customer/customer_detail_controller.dart';
 import 'package:t_ecommerce_admin_panel/features/shop/screens/customer/customer_detail/responsive_screens/customer_detail_desktop.dart';
 import 'package:t_ecommerce_admin_panel/features/shop/screens/customer/customer_detail/responsive_screens/customer_detail_mobile.dart';
 import 'package:t_ecommerce_admin_panel/features/shop/screens/customer/customer_detail/responsive_screens/customer_detail_tablet.dart';
@@ -11,6 +12,7 @@ class CustomerDetailScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final customer = Get.arguments;
+    Get.put(CustomerDetailController());
 
     return TSiteTemplate(
       desktop: CustomerDetailDesktopScreen(customer: customer),
